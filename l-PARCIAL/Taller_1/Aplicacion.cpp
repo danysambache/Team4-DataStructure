35fc8a1dd32c5a12fce72c750412cc668aa90f79
danysambache <dasambache@espe.edu.ec>
2021-05-20 17:55:47 -0500

    DS commit

--- l-PARCIAL/Taller_1/Aplicacion.cpp
+++ l-PARCIAL/Taller_1/Aplicacion.cpp
@@ -1,40 +1,52 @@
-/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
-	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
-    INGENIERA DE SOFTWARE
+/*	UNIVERSITY OF THE ARMED FORCES ESPE
+	COMPUTER SCIENCE DEPARTMENT
+    SOFTWARE ENGINEER
                 
-	AUTORES:    JEAN CARLO CEMBRANOS - jccembranos@espe.edu.ec
+	AUTHORS:    JEAN CARLO CEMBRANOS - jccembranos@espe.edu.ec
 				RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
 				ALEXANDER MAILA - jamaila@espe.edu.ec
 				JIMMY SIMBAÑA - jasimbana14@espe.edu.ec
 				DANNY SAMBACHE - dasambache@espe.edu.ec (LIDER)
 	
-	ENUNCIADO:  SE DESEA SUMAR, RESTAR, MULTIPLICAR Y DIVIDIR DOS NUMEROS UTILIZANDO TEMPLATES, BASADOS EN CLASES.				
+	STATEMENT: YOU WANT TO ADD, SUBTRACT, MULTIPLY AND DIVIDE TWO NUMBERS USING TEMPLATES, BASED ON CLASSES.				
 				
-	FECHA DE CREACION:        19-05-21 
-	FECHA DE MODIFICACION:    -------*/
+	CREATION DATE:        19-05-21 	
+	MODIFICATION DATE:    -------*/
 	
+//LIBRARY STATEMENT	
 #include<iostream>
 #include<stdio.h>
 #include<conio.h>
+
+//CALL TO CLASS FUNCTIONS
 #include"Funciones.cpp"
+
 
 int main(){
 	
+	//ATTRIBUTE STATEMENT
 	float valor1,valor2;
 	Operaciones<float> ope;
-	cout<<"Ingrese el valor 1: "<<endl;
+	
+	//STRUCTURE
+	cout<<"**********************************"<<endl;
+	cout<<"\tBASIC OPERATIONS"<<endl;
+	cout<<"Please, enter the desired number: ";
 	cin>>valor1;
 	ope.setValor1(valor1);
-	cout<<"Ingrese el valor 2: "<<endl;
+	cout<<"Please, enter the desired number: ";
 	cin>>valor2;
 	cout<<endl;
 	ope.setValor2(valor2);
 	
+	//CALL TO METHODS
 	Funciones<float> operacion;
-	cout<<"Suma = "<<operacion.suma(ope)<<endl;	
-	cout<<"Resta = "<<operacion.resta(ope)<<endl;	
-	cout<<"Multiplicacion = "<<operacion.multiplicacion(ope)<<endl;	
-	cout<<"Division = "<<operacion.division(ope)<<endl;	
-	
+	cout<<"---------------------------------"<<endl;
+	cout<<"\tRESULTS"<<endl;
+	cout<<"SUM = "<<"\t\t\t"<<operacion.suma(ope)<<endl;	
+	cout<<"SUBTRACTION = "<<"\t\t"<<operacion.resta(ope)<<endl;	
+	cout<<"MULTIPLICATION = "<<"\t"<<operacion.multiplicacion(ope)<<endl;	
+	cout<<"DIVISION = "<<"\t\t"<<operacion.division(ope)<<endl;	
+	cout<<"---------------------------------";
 	return 0;
 }
