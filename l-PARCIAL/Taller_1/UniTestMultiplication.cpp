0264b68055dec766a5ef1af5758760bdcf8802b4
Jimmy Simbaña <75291241+JimmySimbana99@users.noreply.github.com>
2021-05-20 21:01:08 -0500

    JS commit

---
+++ l-PARCIAL/Taller_1/UniTestMultiplication.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Operations{	
+	private: 
+		float a,b;
+		
+	public:
+		Operations(float, float);
+		
+		float multiplication(float a,float b){
+			return a*b;
+		};					
+};
+
+Operations::Operations(float a_1, float b_2){
+	a=a_1;
+	b=b_2;
+}
+
+int main(){
+	float a,b;
+	cout<<"Please, enter your number: ";
+	cin>>a;
+	cout<<"Please, enter your number: ";
+	cin>>b;
+	
+	Operations ope = Operations(a,b);
+	cout<<"MULTIPLICATION = "<<ope.multiplication(a,b);
+};
+
+
