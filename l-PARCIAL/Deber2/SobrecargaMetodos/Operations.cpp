b59c79fb102ea8cbb7343bfa5ae0f680498c1d17
Jean Cembranos <jccembranos@espe.edu.ec>
2021-05-28 00:21:25 -0500

    JC commit

---
+++ l-PARCIAL/Deber2/SobrecargaMetodos/Operations.cpp
@@ -0,0 +1,74 @@
+/*	UNIVERSITY OF THE ARMED FORCES ESPE
+	COMPUTER SCIENCE DEPARTMENT
+    SOFTWARE ENGINEER
+                
+	AUTHORS:    JEAN CARLO CEMBRANOS - jccembranos@espe.edu.ec
+				RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
+				ALEXANDER MAILA - jamaila@espe.edu.ec
+				JIMMY SIMBANA - jasimbana14@espe.edu.ec
+				DANNY SAMBACHE - dasambache@espe.edu.ec (LIDER)
+	
+	STATEMENT: It is required to overload 3 methods for each member of the group.				
+				
+	CREATION DATE:        27-05-21 	
+	MODIFICATION DATE:    27-05-21*/
+#include "Operations.h"
+#include <iostream>
+using namespace std;
+/**
+ * @brief Construct a new Operations:: Operations object
+ * 
+ * @param data_D 
+ */
+Operations::Operations(Data data_D)
+{
+	this->data=data_D;
+}
+/**
+ * @brief Construct a new Operations:: Operations object
+ * 
+ */
+Operations::Operations(){
+	
+}
+/**
+ * @brief Destroy the Operations:: Operations object
+ * 
+ */
+Operations::~Operations()
+{
+   // TODO : implement
+}
+/**
+ * @brief This function add three numbers
+ * 
+ * @param val_1 
+ * @param val_2 
+ * @param val_3 
+ * @return float 
+ */
+float Operations::add(float val_1,int val_2,float  val_3){
+	return (int)val_1+val_2+(int)val_3;
+}
+/**
+ * @brief This function add two numbers
+ * 
+ * @param val_1 
+ * @param val_2 
+ * @return int 
+ */
+int Operations::add(int val_1,int val_2){
+	return val_1+val_2;
+}
+/**
+ * @brief This function add four numbers
+ * 
+ * @param val_1 
+ * @param val_2 
+ * @param val_3 
+ * @param val_4 
+ * @return int 
+ */
+int Operations::add(int val_1,float val_2,int val_3,float val_4){
+	return val_1+(int)val_2+val_3+(int)val_4;
+}
