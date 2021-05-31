252b9ff9e0f3f60f7f007147e57659fb2ecab551
Jean Cembranos <jccembranos@espe.edu.ec>
2021-05-30 19:39:44 -0500

    JC Changes

---
+++ l-PARCIAL/Deber3/Data.h
@@ -0,0 +1,27 @@
+/*	UNIVERSITY OF THE ARMED FORCES ESPE
+	COMPUTER SCIENCE DEPARTMENT
+    SOFTWARE ENGINEER
+                
+	AUTHORS:    JEAN CARLO CEMBRANOS - jccembranos@espe.edu.ec
+				RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
+				ALEXANDER MAILA - jamaila@espe.edu.ec
+				JIMMY SIMBANA - jasimbana14@espe.edu.ec
+				DANNY SAMBACHE - dasambache@espe.edu.ec (LIDER)
+	
+	STATEMENT: Requires overloading operators, using templates and classes				
+				
+	CREATION DATE:        28-05-21 	
+	MODIFICATION DATE:    30-05-21*/
+//#pragma once
+template<typename T>
+class Data
+{
+public:
+	Data()=default;
+	Data(T vect[10]);
+	~Data();
+	T *getVect(void);
+	void setVect(T vect[10]);
+private:	
+	T vect[10];
+};
