13bc26c78779eecd780f72f3eb1bac1c02e9b5f0
joseAle10k <jamaila@espe.edu.ec>
2021-05-30 19:50:53 -0500

    AM commit

---
+++ l-PARCIAL/Deber3/Operations.h
@@ -0,0 +1,25 @@
+/*	UNIVERSITY OF THE ARMED FORCES ESPE
+	COMPUTER SCIENCE DEPARTMENT
+    SOFTWARE ENGINEER
+                
+	AUTHORS:    JEAN CARLO CEMBRANOS - jccembranos@espe.edu.ec
+				RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
+				ALEXANDER MAILA - jamaila@espe.edu.ec
+				JIMMY SIMBANA - jasimbana14@espe.edu.ec
+				DANNY SAMBACHE - dasambache@espe.edu.ec (LIDER)
+	
+	STATEMENT: Requires overloading operators, using templates and classes				
+				
+	CREATION DATE:        28-05-21 	
+	MODIFICATION DATE:    30-05-21*/
+#pragma once
+template<typename H>
+class Operations
+{
+public:
+	Operations();
+	void encerar(H vect[]);
+	void generar(H vect[]);
+	float procesar(H vect[]);
+	void imprimir(H vect[]);
+};
