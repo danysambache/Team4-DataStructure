d440b5f64be2214bd6076b929ab22cbb29ef8dd3
danysambache <dasambache@espe.edu.ec>
2021-06-03 23:26:04 -0500

    DS commit

--- l-PARCIAL/MatricesPow/Code/main.cpp
+++ l-PARCIAL/MatricesPow/Code/main.cpp
@@ -1,7 +1,45 @@
+/*  UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+    DEPARTAMENTO DE CIENCIAS DE LA COMPUTACIÓN
+    INGENIERÍA DE SOFTWARE
+
+    AUTORES:    JEAN CARLO CEMBRANOS - jccembranos@espe.edu.ec
+                RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
+                ALEXANDER MAILA - jamaila@espe.edu.ec
+                JIMMY SIMBAÃ‘A - jasimbana14@espe.edu.ec
+                DANNY SAMBACHE - dasambache@espe.edu.ec (LIDER)
+
+    STATEMENT:  IMPLEMENTAR LA FUNCION MATRIZ A LA N-ESIMA MEDIANTE PUNTEROS
+
+    CREATION DATE:        02-06-21
+    MODIFICATION DATE:    03-06-21*/
 #include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+#include "Matrices.h"
+#include "Operaciones.h"
+#include <Math.h>
+using namespace std;
 int main(int argc, char** argv) {
+	int dim,n;
+	int **mat1, **mat2, **matr;
+	cout<<"****************************************"<<endl;
+	cout<<"Ingrese la dimension de la matriz: ";
+	cin>>dim;
+	cout<<endl;
+	Operaciones op;
+	mat1 = op.segmentar(dim);
+	mat2 = op.segmentar(dim);
+	matr = op.segmentar(dim);
+	op.encerar(mat1, dim);
+	op.encerar(mat2, dim);
+	op.encerar(matr, dim);
+	cout<<"Ingrese datos Matriz 1: "<<endl;
+	op.ingresar(mat1, dim);
+	cout<<endl;
+	cout<<"Ingrese la potencia para la matriz: ";
+	cin>>n;
+	cout<<endl;
+	op.calcular(mat1,mat2,matr,dim,n);
+	cout<<"Resultado: "<<endl;
+	op.imprimir(mat2,dim);	
+	cout<<endl;
 	return 0;
 }
