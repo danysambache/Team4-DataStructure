55dca2c2c2240357f859710130954edbc06ed355
Jean Cembranos <jccembranos@espe.edu.ec>
2021-06-06 17:53:11 -0500

    JC commit

--- l-PARCIAL/DeberFuncionesTrigo/code/main.cpp
+++ l-PARCIAL/DeberFuncionesTrigo/code/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
 	int valor,valAprox;
 	float res;
-	cout<<"Ingrese la potencia aproximada: ";
+	cout<<"Ingrese el valor de aproximacion (50 o mas para mayor aproximacion): ";
 	cin>>valAprox;
 	cout << "Ingrese el valor del f(x): ";
 	cin>>valor;
 	cout<<endl;
 	Operaciones op;
-	//Cos
+	//Cos Jean Carlo Cembranos
 	res=op.calc_cos(valAprox,valor);
 	cout.setf(ios::fixed);
     cout.precision(9);
 	cout<<"Resultado Cos(x): "<<res<<endl;
-	//Csc
+	//Csc Danny Sambache
 	res=op.calc_csc(valAprox,valor);
 	cout.setf(ios::fixed);
     cout.precision(9);
