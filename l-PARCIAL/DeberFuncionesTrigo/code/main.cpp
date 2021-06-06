19fdfbfb9240b0d8873c216bd000d8bcfa525d3f
danysambache <dasambache@espe.edu.ec>
2021-06-06 17:49:04 -0500

    DS commit

--- l-PARCIAL/DeberFuncionesTrigo/code/main.cpp
+++ l-PARCIAL/DeberFuncionesTrigo/code/main.cpp
@@ -4,14 +4,21 @@ using namespace std;
 int main(){
 	int valor,valAprox;
 	float res;
-	cout<<"Ingrese la potencia aproximada: "<<endl;
+	cout<<"Ingrese la potencia aproximada: ";
 	cin>>valAprox;
-	cout << "Ingrese el valor del cos(x): "<<endl;
+	cout << "Ingrese el valor del f(x): ";
 	cin>>valor;
+	cout<<endl;
 	Operaciones op;
+	//Cos
 	res=op.calc_cos(valAprox,valor);
 	cout.setf(ios::fixed);
     cout.precision(9);
-	cout<<"resultado Cos(x): "<<res<<endl;
+	cout<<"Resultado Cos(x): "<<res<<endl;
+	//Csc
+	res=op.calc_csc(valAprox,valor);
+	cout.setf(ios::fixed);
+    cout.precision(9);
+	cout<<"Resultado Csc(x): "<<res<<endl;
 	return 0;
 }
